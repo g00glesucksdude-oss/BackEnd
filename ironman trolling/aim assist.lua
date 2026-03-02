25e9b41279ed569c6c0ec8e764730355b4e38c05
g00glesucksdude-oss <g00glesucksdude@gmail.com>
2026-03-02 19:18:20 +0800

    Update aim assist.lua

--- ironman trolling/aim assist.lua
+++ ironman trolling/aim assist.lua
@@ -1,2 +1,2 @@
-local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
+local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/g00glesucksdude-oss/BackEnd/refs/heads/main/not%20needed/aim%20assist.lua"))()
 Aimbot.Load()
